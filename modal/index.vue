c4135b0ccd91b0a8cf153688b3458034ef58dd81
曹世通 <caoshitong014@pingan.com.cn>
2017-06-02 11:40:12 +0800

    vpui commit

---
+++ modal/index.vue
@@ -0,0 +1,59 @@
+<template>
+<Shade :visible="visibility" :fx="true">
+    <Overlay :visible="true" :class="className" position="center">
+        <section class="vmui-modal-header">
+            <slot name="header"></slot>
+        </section>
+        
+        <section class="vmui-modal-content">
+            <slot></slot>
+        </section>
+       
+        <section class="vmui-modal-footer">
+            <slot name="footer"></slot>
+        </section>
+    </Overlay>
+</Shade>
+</template>
+
+<style>
+.vmui-modal{
+    border-radius: 4px;
+    width: 60%;
+    padding: 16px 10px;
+}
+
+.vmui-modal-footer{
+    margin-top: 14px;
+    text-align: center;
+}
+</style>
+
+<script>
+import Overlay from '../overlay';
+import Shade from '../shade';
+
+export default{
+    mixins: [Overlay],
+
+    props: {
+        buttons: {
+            type: Object,
+            default(){
+                return {}
+            }
+        }
+    },
+
+    components: {
+        Overlay,
+        Shade
+    },
+
+    computed: {
+        className(){
+            return ('vmui-modal ' + (this.class || '')).trim()
+        }
+    }
+}
+</script>
